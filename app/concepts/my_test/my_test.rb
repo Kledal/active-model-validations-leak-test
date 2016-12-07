4c611efe2d0faf31c1137734df24073ee56573fe
Alexander Kledal <201371052@iha.dk>
2016-12-07 14:26:59 +0100

    Use activemodel validations

---
+++ app/concepts/my_test/my_test.rb
@@ -0,0 +1,18 @@
+class MyTest
+  class MyTest < Trailblazer::Operation
+    contract do
+      property :test, virtual: true
+
+      validates :test, presence: true
+
+      #validation :default do
+      #  required(:test).filled
+      #end
+    end
+
+    def process(params)
+      validate(params) do
+      end
+    end
+  end
+end
